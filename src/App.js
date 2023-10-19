ef837da51e913d10a14f07e4abc4a60a4f35b334
Tayyab <tayyab.21bce7333@vitapstudent.ac.in>
2023-10-19 21:08:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [age, setAge] = useState(null);
+
+  const calculateAge = () => {
+    const inputDate = document.getElementById("inp").value;
+    const birthDate = new Date(inputDate);
+    const currentDate = new Date();
+
+    if (!isNaN(birthDate)) {
+      const ageInMilliseconds = currentDate - birthDate;
+      const ageInYears = ageInMilliseconds / (1000 * 60 * 60 * 24 * 365.25);
+      setAge(Math.floor(ageInYears));
+    } else {
+      setAge("Invalid date");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Age Calculator</h1>
+      <br />
+      <h5>Enter your date of birth</h5>
+      <br />
+      <input type="date" id="inp" name="inp" />
+      <br />
+      <button
+        id="btn"
+        style={{ color: "white", backgroundColor: "blue" }}
+        onClick={calculateAge}
+      >
+        Calculate Age
+      </button>
+      <br />
+      <span id="res" name="res">
+        {age !== null ? `Your age is ${age} years.` : ""}
+      </span>
     </div>
   );
 }
